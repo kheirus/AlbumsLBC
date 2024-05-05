dc2f72c2311e6f1f566cde026de7bc14926852c5
Kheireddine Ouelaa <kheireddine.ouelaa@hotmail.com>
2024-05-05 02:31:38 +0200

    Initial commit albums LBC test

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+}
