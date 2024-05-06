f10eb8f7249fcbd5ac14234313de1cf87442a932
Kheireddine Ouelaa <kheireddine.ouelaa@hotmail.com>
2024-05-07 00:24:13 +0200

    tech(hilt): Add Hilt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,5 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
 }
