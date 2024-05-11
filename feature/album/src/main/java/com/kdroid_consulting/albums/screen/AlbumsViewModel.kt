4ead6c6572b127ff5aa08ff1d9e464539ccb6f1b
Kheireddine Ouelaa <kheireddine.ouelaa@hotmail.com>
2024-05-11 22:28:07 +0200

    tech(archi): Move usecase package to the correct place

--- feature/album/src/main/java/com/kdroid_consulting/albums/screen/AlbumsViewModel.kt
+++ feature/album/src/main/java/com/kdroid_consulting/albums/screen/AlbumsViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kdroid_consulting.common.Result
 import com.kdroid_consulting.domain.model.Album
-import com.kdroid_consulting.domain.repository.usecase.GetAlbumsUseCase
+import com.kdroid_consulting.domain.usecase.GetAlbumsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
